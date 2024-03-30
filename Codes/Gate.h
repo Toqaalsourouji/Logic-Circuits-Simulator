3a50e8418d87b531005830ef2fd1d9b4625d914d
Adham Gohar <125025049+Adham-Gohar@users.noreply.github.com>
2024-03-30 22:25:51 +0200

    Update Gate.h

--- Codes/Gate.h
+++ Codes/Gate.h
@@ -1,3 +1,4 @@
+#pragma once
 #ifndef GATE_H
 #define GATE_H
 
@@ -6,20 +7,22 @@
 #include <iostream>
 #include <unordered_map>
 
+using namespace std;
+
 struct Gate {
-    std::string type;
-    std::vector<std::string> inputs;
-    std::string output; // Output signal name
-    std::string outputExpr; // Logical expression for output
+    string type;
+    vector<string> inputs;
+    string output; // Output signal name
+    string outputExpr; // Logical expression for output
     int delay;
 
     // Constructor is not necessary but helpful for initializing Gate objects cleanly
-    Gate(std::string t = "", std::vector<std::string> i = {}, std::string o = "", std::string oe = "", int d = 0)
+    Gate(string t = "", vector<string> i = {}, string o = "", string oe = "", int d = 0)
         : type(t), inputs(i), output(o), outputExpr(oe), delay(d) {}
 };
 
-extern std::unordered_map<std::string, Gate> gates;
-extern std::unordered_map<std::string, std::vector<std::string>> signalToGates;
+extern unordered_map<string, Gate> gates;
+extern unordered_map<string, vector<string>> signalToGates;
 
 
 #endif // GATE_H
